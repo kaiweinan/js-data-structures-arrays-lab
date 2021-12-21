ef5b07b415449c3d2e81aefd4588367570380608
kaiweinan <kaiweinan@gmail.com>
2021-12-21 16:55:49 -0500

    commit tree ed8a9f0d97c83abdae535e9703d975d604bdfc1b

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution here!
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function Append(name){
+    kittens.push(name)
+    return kittens
+  }
+  function Prepend(name){
+    kittens.unshift(name)
+    return kittens
+  }
+  function RemoveLast(){
+    kittens.pop()
+  }
+  function RemoveFirst(){
+    kittens.shift()
+  }
+  function append(name){
+    return [...kittens,name]
+  
+  }
+  function prepend(name){
+    return [name,...kittens]
+  }  
+  function removeLast(){
+    var new_kittens  = kittens.slice(0,-1)
+    return new_kittens
+  }
+  function removeFirst(){
+    var new_kittens = kittens.slice(1)
+    return new_kittens
+  }
